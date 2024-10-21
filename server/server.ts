04def82e3a571b8ee372be2d8213f5c19e30ee02
gillancodes <gillancodes@laposte.net>
2024-10-21 13:48:54 +0200

    feat: added base

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,20 @@
 import * as express from "express";
+import * as http from "http";
+
+import * as bodyParser from "body-parser";
+import * as cookieParser from "cookie-parser";
+
 
 let app:express.Application = express();
+let server = http.createServer(app);
 
-app.listen(5700, ():void => {
+app.use(bodyParser.urlencoded({limit:"5mb", extended:true, parameterLimit:50}));
+app.use(bodyParser.json({limit:"5mb"}));
+
+app.use(cookieParser());
+
+
+
+server.listen(5700, ():void => {
   console.log("App listen on 5700")
 });
