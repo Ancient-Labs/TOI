8af4a6f8cdab71379052f20e25c0ae0b7de9d449
gillancodes <gillancodes@laposte.net>
2024-10-24 20:16:21 +0200

    feat: added auth middleware

---
+++ api/middlewares/auth.middleware.ts
@@ -0,0 +1,48 @@
+import config from "../config/config.ts";
+import * as jwt from 'jsonwebtoken';
+import userModel from "../models/user.model.ts";
+import express from 'express';
+
+type decodedTokenType = {
+    id: string
+}
+
+export function checkUser(req: express.Request, res: express.Response, next: () => void)
+{
+    const token = req.cookies.auth;
+
+    if (!token) 
+    {
+        res.locals.user = null;
+        next();
+    }
+
+    jwt.verify(token, config.JWT_TOKEN, async(err: string | object, decodedToken: decodedTokenType) => {
+        if (err)
+        {
+            res.locals.user = null;
+            next();
+        } else {
+            const user = await userModel.findById(decodedToken.id).select('-password');
+            res.locals.user = user;
+            next();
+        }
+    })
+}
+
+export function requireAuth(req: express.Request, res: express.Response, next: () => void)
+{
+    const token = req.cookies.auth;
+
+    if(!token) return res.status(200);
+    
+    jwt.verify(token, config.JWT_TOKEN, (err: string, decodedToken: decodedTokenType) => {
+        if (err)
+        {
+            console.log(err);
+        } else {
+            next();
+        }
+    })
+
+}
