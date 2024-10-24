b358cd6a6e36fd3a340d43e22a3d0d211ba1f069
gillancodes <gillancodes@laposte.net>
2024-10-24 13:13:59 +0200

    refactor: fix some typo

--- api/server.ts
+++ api/server.ts
@@ -1,5 +1,5 @@
 import express from "npm:express";
-import * as http from "node:http";
+import http from "node:http";
 
 import bodyParser from "npm:body-parser";
 import cookieParser from "npm:cookie-parser";
@@ -8,8 +8,8 @@ import cookieParser from "npm:cookie-parser";
 import config from "./config/config.ts";
 import log from "./log.ts";
 
-let app:express.Application = express();
-let server = http.createServer(app);
+const app:express.Application = express();
+const server:http.Server = http.createServer(app);
 
 app.use(bodyParser.urlencoded({limit:"5mb", extended:true, parameterLimit:50}));
 app.use(bodyParser.json({limit:"5mb"}));
