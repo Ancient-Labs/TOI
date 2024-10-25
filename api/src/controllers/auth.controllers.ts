794e30cfcd1c4d5bf8763e6fe09b6aa1b99da3f6
gillancodes <gillancodes@laposte.net>
2024-10-25 13:01:57 +0200

    feat: added auth controllers and routes

---
+++ api/src/controllers/auth.controllers.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "npm:express";
+
+export async function signup(req:Request, res:Response): Promise<void>
+{
+
+}
+
+export async function signin(req:Request, res:Response): Promise<void>
+{
+
+}
+
+export function signout(req:Request, res: Response): void
+{
+    
+}
---
+++ api/src/routes/auth.routes.ts
@@ -0,0 +1,7 @@
+import { Router } from "npm:express";
+
+const router:Router = Router();
+
+router.post('/signup')
+router.post('/signin')
+router.post('/signout')
