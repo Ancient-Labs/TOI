a04f656ccfb1b45b4251987f133f84354a1581e9
gillancodes <gillancodes@laposte.net>
2024-10-25 13:59:34 +0200

    feat: auth router done

--- api/src/routes/auth.routes.ts
+++ api/src/routes/auth.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "npm:express";
 
+// Import controllers.
+import { signin, signout, signup } from "../controllers/auth.controllers.ts";
+
 const router:Router = Router();
 
-router.post('/signup')
-router.post('/signin')
-router.post('/signout')
+router.post('/signup', signup)
+router.post('/signin', signin)
+router.post('/signout', signout)
+
+export default router;
